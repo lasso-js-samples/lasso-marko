afc7498f36335772fd3c350a8c4fe33fe2884b87
Patrick Steele-Idem <pnidem@gmail.com>
2015-08-26 14:46:53 -0600

    Set URL prefix

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./node_modules/.bin/lasso --main client.js --plugins lasso-marko --name bundle --inject-into index.html
+./node_modules/.bin/lasso --main client.js --plugins lasso-marko --name bundle --inject-into index.html --url-prefix "./static"
